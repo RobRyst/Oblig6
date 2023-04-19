0a1f07f246adf3079e64a215e5418ca065e7fc44
Ryztard <72693058+Ryztard@users.noreply.github.com>
2023-04-19 11:09:00 +0200

    Create WhaleShark.java

---
+++ WhaleShark.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class WhaleShark extends Animals {
+
+    private double runDistanceInKm;
+    private double runSpeedInKmPerHour;
+    public WhaleShark(String name, String animalType, DietType dietType, int age, double runDistanceInKm, double runSpeedInKmPerHour) {
+        super(name, animalType, dietType, age);
+        this.runDistanceInKm = runDistanceInKm;
+        this.runSpeedInKmPerHour = runSpeedInKmPerHour;
+    }
+
+    @Override
+    public double calculateSpeed() {
+        return runDistanceInKm / runSpeedInKmPerHour + getAge();
+    }
+
+
+    @Override
+    public String toString() {
+        return "The name of the " + getAnimalType() + " is " + getName() + " ,and they are considered a " + getDietType() +
+                " and the animals " + getName() + " is " + getAge() + " years old" + " The animal runs at a " +
+                calculateSpeed() + " km/h"
+                ;
+    }
+}
